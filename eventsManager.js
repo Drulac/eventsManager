67365cc09a85b24779dafaa9d8abc1cfbf8f234b
Drulac <dev.drulac@gmail.com>
2016-10-23 22:32:54 +0200

    Create eventsManager.js

---
+++ eventsManager.js
@@ -0,0 +1,31 @@
+let _eventManager = function()
+{
+	let self = this;
+	let events = {};
+
+	self.on = function(event, func)
+	{
+		if(typeof events[event] != "array")
+		{
+			events[event] = [];
+		}
+		events[event].push(func);
+
+		return self;
+	};
+
+	self.run = function(event, data)
+	{
+		if(typeof events[event] == "array" || typeof events[event] == "object")
+		{
+			for(let i = 0, ni = events[event].length; i < ni; i++)
+			{
+				setTimeout(function(){
+					events[event][i].apply(null, data);
+				}, 1);
+			}
+		}
+
+		return self;
+	};
+};
